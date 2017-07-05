3bf38cfad1d0aecf0708bd6b2d6df48dac8f5e7f
William Ahern <william@25thandclement.com>
2017-07-05 14:58:03 -0700

    keep cache of checksums so we can better handle hardlinked files in the tarball

--- tarsum.c
+++ tarsum.c
@@ -12,6 +12,11 @@
 #include <openssl/err.h>
 #include <openssl/evp.h>
 
+#include "llrb.h"
+
+#undef MIN
+#define MIN(a, b) (((a) < (b))? (a) : (b))
+
 #if !HAVE_EVP_MD_CTX_NEW
 #define EVP_MD_CTX_new(md) EVP_MD_CTX_create()
 #endif
@@ -24,6 +29,87 @@ static const char *
 openssl_error_string(void)
 {
 	return "OpenSSL error";
+}
+
+static void
+SHA256(void *dst, size_t lim, const void *src, size_t len)
+{
+	static EVP_MD_CTX *ctx;
+	unsigned char md[32];
+	unsigned mdlen;
+	if (!ctx && !(ctx = EVP_MD_CTX_new()))
+		errx(1, "%s", openssl_error_string());
+	if (!EVP_DigestInit_ex(ctx, EVP_sha256(), NULL))
+		errx(1, "%s", openssl_error_string());
+	if (!EVP_DigestUpdate(ctx, src, len))
+		errx(1, "%s", openssl_error_string());
+	if (!EVP_DigestFinal_ex(ctx, md, &mdlen))
+		errx(1, "%s", openssl_error_string());
+	memcpy(dst, md, MIN(mdlen, lim));
+}
+
+static char *
+md2hex(const void *_src, size_t len)
+{
+	static unsigned char md[(2 * EVP_MAX_MD_SIZE) + 1];
+	struct { unsigned char *p, *pe; } src, dst;
+
+	src.p = (void *)_src;
+	src.pe = src.p + MIN(((sizeof md - 1) / 2), len);
+	dst.p = md;
+
+	for (; src.p < src.pe; src.p++) {
+		*dst.p++ = "0123456789abcdef"[0x0f & (*src.p >> 4)];
+		*dst.p++ = "0123456789abcdef"[0x0f & (*src.p >> 0)];
+	}
+	*dst.p = '\0';
+
+	return (char *)md;
+}
+
+struct entry {
+	unsigned char name[32]; /* SHA256 digest length */
+	unsigned char md[EVP_MAX_MD_SIZE];
+	unsigned mdlen;
+	char path[256];
+	LLRB_ENTRY(entry) rbe;
+};
+
+static LLRB_HEAD(entries, entry) entries;
+
+static int
+entrycmp(const struct entry *a, const struct entry *b)
+{
+	return memcmp(a->name, b->name, sizeof a->name);
+}
+
+LLRB_GENERATE_STATIC(entries, entry, rbe, entrycmp);
+
+static void
+entryadd(const char *path, const void *md, size_t mdlen)
+{
+	struct entry *ent, *oent;
+	if (!(ent = malloc(sizeof *ent)))
+		err(1, "malloc");
+	memset(ent, 0, sizeof *ent);
+	SHA256(ent->name, sizeof ent->name, path, strlen(path));
+	memcpy(ent->md, md, MIN(mdlen, sizeof ent->md));
+	ent->mdlen = mdlen;
+	strlcpy(ent->path, path, sizeof ent->path);
+	if ((oent = LLRB_INSERT(entries, &entries, ent))) {
+		warnx("duplicate (%s) (%s)", path, ent->path);
+		warnx("  %s", md2hex(ent->name, sizeof ent->name));
+		warnx("  %s", md2hex(oent->name, sizeof oent->name));
+		free(ent);
+	}
+}
+
+static const struct entry *
+entryget(const char *path)
+{
+	struct entry key;
+	SHA256(key.name, sizeof key.name, path, strlen(path));
+	return LLRB_FIND(entries, &entries, &key);
 }
 
 static long
@@ -85,11 +171,18 @@ main(int argc, char **argv)
 	while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
 		const char *path = archive_entry_pathname(entry);
 		unsigned char md[EVP_MAX_MD_SIZE];
-		char mdx[(2 * sizeof md) + 1];
 		unsigned mdlen;
 		EVP_MD_CTX *ctx;
 		const void *buf;
 		size_t buflen, i;
+
+		if (archive_entry_hardlink(entry)) {
+			const struct entry *ent = entryget(archive_entry_hardlink(entry));
+			if (ent) {
+				printf("%s  %s\n", md2hex(ent->md, ent->mdlen), path);
+			}
+			continue;
+		}
 
 		if (!(ctx = EVP_MD_CTX_new()) || !EVP_DigestInit_ex(ctx, algo, NULL))
 			errx(1, "%s", openssl_error_string());
@@ -111,13 +204,9 @@ main(int argc, char **argv)
 		if (!EVP_DigestFinal_ex(ctx, md, &mdlen))
 			errx(1, "%s", openssl_error_string());
 		EVP_MD_CTX_free(ctx);
-		for (i = 0; i < mdlen; i++) {
-			mdx[(2 * i) + 0] = "0123456789abcdef"[0x0f & (md[i] >> 4)];
-			mdx[(2 * i) + 1] = "0123456789abcdef"[0x0f & (md[i] >> 0)];
-		}
-		mdx[mdlen * 2] = '\0';
 
-		printf("%s  %s\n", mdx, archive_entry_pathname(entry));
+		printf("%s  %s\n", md2hex(md, mdlen), path);
+		entryadd(path, md, mdlen);
 	}
 	if (archive_errno(a) != ARCHIVE_OK)
 		err(1, "%s", archive_error_string(a));
